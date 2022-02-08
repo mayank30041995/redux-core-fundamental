d70fbdf821bb2f69e78bf20cf09da1f6039d9047
Mayank Sharma <mayankece155@gmail.com>
2022-02-08 14:17:00 +0530

    more action on custom store

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import * as actions from './action';
 store.dispatch(actions.bugAdded("BUG 1"));
 
 console.log(store.getState()); 
+
+store.dispatch(actions.bugAdded("BUG 5"));
+
+console.log(store.getState()); 
+
+store.dispatch(actions.bugRemoved(1));
+
+console.log(store.getState());
+
+store.dispatch(actions.bugResolved(2));
+
+console.log(store.getState());
