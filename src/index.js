f6f6426dd6775e58c7a95885360b2ffd779379c1
Mayank Sharma <mayankece155@gmail.com>
2022-02-08 09:06:29 +0530

    defining action reducer & store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import { produce } from "immer";
+import store from './store';
 
-let book = { title: "Harry Potter", section: { data: "HSIJ"} };
-
-function publish(book) {
-  return produce(book, draftBook => {
-    draftBook.section.data = "BOKA";
-  });
-}
-
-let updated = publish(book);
-
-console.log(book);
-console.log(updated);
+console.log(store)
---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+let lastId=0;
+
+export default function reducer(state=[], action) {
+switch (action.type) {
+    case "bugAdded" : 
+    return [
+        ...state,
+        {
+            id : ++lastId,
+            description : action.payload.description,
+            resolved: false
+        }
+    ]
+}}
