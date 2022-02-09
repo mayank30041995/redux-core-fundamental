50d107c0762b64ba3a6181614cc617b1a5f738b4
Mayank Sharma <mayankece155@gmail.com>
2022-02-09 18:04:05 +0530

    create reducers using redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 const action = createAction("bugUpdated");
 console.log(action.toString());
@@ -15,23 +15,23 @@ export const bugResolved=  createAction("bugResolved")
 
 // reducers
 
-export default function reducer(state=[], action) {
-    switch (action.type) {
-        case bugAdded.type : 
-        return [
-            ...state,
-            {
-                id : ++lastId,
-                description : action.payload.description,
-                resolved: false
-            }
-        ]
-    
-        case bugRemoved.type :
-            return  state.filter( bug => bug.id !== action.payload.id)
-    
-        case bugResolved.type :
-             return state.map(bug => (bug.id !== action.payload.id) ? bug : {...bug, resolved: true})
-    
-        default: return state;
-    }}
+export default createReducer([], {
+
+    [bugAdded.type]: (bugs , action) => {
+        bugs.push({
+            id: ++lastId,
+            description: action.payload.description,
+            resolved: false
+        })
+    },
+
+    [bugRemoved.type] : (bugs , action) => {
+        return bugs.filter(bug => bug.id !== action.payload.id);
+    },
+
+    [bugResolved.type] : (bugs , action) => {
+        const index = bugs.findIndex(bug => bug.id === action.payload.id);
+        bugs[index].resolved = true;
+    }
+
+})
